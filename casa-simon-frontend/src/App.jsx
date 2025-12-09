277af6aedd50a049d07805803005ee9dd8da6d94
nicolaswer <135063538+nicolaswer@users.noreply.github.com>
2025-12-09 15:50:17 +0100

    Fix import path for ScrollToTop component

--- casa-simon-frontend/src/App.jsx
+++ casa-simon-frontend/src/App.jsx
@@ -9,7 +9,7 @@ import Menu from './components/Menu';
 import { APP_ROUTES } from './config/routes';
 
 // 1. IMPORTA EL COMPONENTE NUEVO
-import ScrollToTop from './components/ScrollToTop'; 
+import ScrollToTop from './components/scrollToTop'; 
 
 import { parallevarData } from './data/parallevarData'; 
 import { restauranteData } from './data/restauranteData'; 
